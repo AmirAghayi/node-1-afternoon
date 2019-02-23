1c293b079f21f44bb9571e0443e569febc14de5f
samskeen <sskeen9@gmail.com>
2019-02-23 10:39:56 -0700

    Chat-Server

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mc = require('./controllers/message_controller');
+
+
+const app = express();
+
+app.use( bodyParser.json() );
+app.use( express.static( __dirname + '/../public/build' ) );
+
+
+const messageBaseUrl = "/api/messages";
+app.post( messageBaseUrl, mc.create );
+app.get( messageBaseUrl, mc.read );
+app.put( `${messageBaseUrl}/:id`, mc.update );
+app.delete( `${messageBaseUrl}/:id`, mc.delete );
+
+
+
+const port = 3001;
+app.listen( port, () => {
+    console.log(
+        'Server listening on port ${port}'
+        );
+    }
+);
